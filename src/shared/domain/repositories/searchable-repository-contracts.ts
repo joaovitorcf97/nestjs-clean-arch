6257e3a34441534ba3033fa6587d9cd766604d03
João Vitor <joaovitorcfdev@gmail.com>
2023-08-07 16:13:18 -0300

    refactoring UserRepository

--- src/shared/domain/repositories/searchable-repository-contracts.ts
+++ src/shared/domain/repositories/searchable-repository-contracts.ts
@@ -21,14 +21,14 @@ export type SearchResultProps<E extends Entity, Filter> = {
   filter: Filter | null;
 };
 
-export class SearchParams {
+export class SearchParams<Filter = string> {
   protected _page: number;
   protected _perPage = 15;
   protected _sort: string | null;
   protected _sortDir: SortDirection | null;
-  protected _filter: string | null;
+  protected _filter: Filter | null;
 
-  constructor(props: SearchProps) {
+  constructor(props: SearchProps<Filter> = {}) {
     this._page = props.page;
     this._perPage = props.perPage;
     this._sort = props.sort;
@@ -89,13 +89,15 @@ export class SearchParams {
     this._sortDir = dir !== 'asc' && dir !== 'desc' ? 'desc' : dir;
   }
 
-  get filter() {
+  get filter(): Filter | null {
     return this._filter;
   }
 
-  private set filter(value: string | null) {
+  private set filter(value: Filter | null) {
     this._filter =
-      value === null || value === undefined || value === '' ? null : `${value}`;
+      value === null || value === undefined || value === ''
+        ? null
+        : (`${value}` as any);
   }
 }
 
@@ -136,9 +138,10 @@ export class SearchResult<E extends Entity, Filter = string> {
 
 export interface SearchableRepositoryInterface<
   E extends Entity,
-  SearchInput,
-  SearchOutput,
+  Filter = string,
+  SearchInput = SearchParams<Filter>,
+  SearchOutput = SearchResult<E, Filter>,
 > extends RepositoryInterface<E> {
   sortableFields: string[];
-  search(props: SearchParams): Promise<SearchOutput>;
+  search(props: SearchInput): Promise<SearchOutput>;
 }
--- src/users/domain/repositories/user.repository.ts
+++ src/users/domain/repositories/user.repository.ts
@@ -1,8 +1,25 @@
 import { UserEntity } from '../entities/user.entity';
-import { SearchaleRepositoryInterface } from '@/shared/domain/repositories/searchable-repository-contracts';
+import {
+  SearchParams as DefaultSearchParam,
+  SearchResult as DefaultSearchResult,
+  SearchableRepositoryInterface,
+} from '@/shared/domain/repositories/searchable-repository-contracts';
 
-export interface UserRepository
-  extends SearchaleRepositoryInterface<UserEntity, any, any> {
-  findByEmail(email: string): Promise<UserEntity>;
-  emailExists(email: string): Promise<void>;
+export namespace UserRepository {
+  export type Filter = string;
+
+  export class SearchParams extends DefaultSearchParam<Filter> {}
+
+  export class SearchResult extends DefaultSearchResult<UserEntity, Filter> {}
+
+  export interface Repository
+    extends SearchableRepositoryInterface<
+      UserEntity,
+      Filter,
+      SearchParams,
+      SearchResult
+    > {
+    findByEmail(email: string): Promise<UserEntity>;
+    emailExists(email: string): Promise<void>;
+  }
 }
