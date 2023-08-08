2234a6ba4f99ebb68013a7b17d70dcdee2a6e780
João Vitor <joaovitorcfdev@gmail.com>
2023-08-08 12:05:29 -0300

    adding SigninUserUseCase

---
+++ src/users/application/usecases/signin.usecase.ts
@@ -0,0 +1,41 @@
+import { UserRepository } from '@/users/domain/repositories/user.repository';
+import { BadRequestError } from '../../../shared/application/errors/bad-request-error';
+import { HashProvider } from '@/shared/application/providers/hash-provider';
+import { UserOutput, UserOutputMapper } from '../dto/user-output';
+import { UseCase as DefaultUseCase } from '@/shared/application/usecases/use-case';
+import { InvalidCredentialsError } from '@/shared/application/errors/invalid-credentials-error';
+
+export namespace SigninUseCase {
+  export type Input = {
+    email: string;
+    password: string;
+  };
+
+  export class UseCase implements DefaultUseCase<Input, UserOutput> {
+    constructor(
+      private userRepository: UserRepository.Repository,
+      private hashProvider: HashProvider,
+    ) {}
+
+    async execute(input: Input): Promise<UserOutput> {
+      const { email, password } = input;
+
+      if (!email || !password) {
+        throw new BadRequestError('Input data no provided');
+      }
+
+      const entity = await this.userRepository.findByEmail(email);
+
+      const hashPasswordMatches = await this.hashProvider.compareHash(
+        password,
+        entity.password,
+      );
+
+      if (!hashPasswordMatches) {
+        throw new InvalidCredentialsError('Invalid credentials');
+      }
+
+      return UserOutputMapper.toOutput(entity);
+    }
+  }
+}
