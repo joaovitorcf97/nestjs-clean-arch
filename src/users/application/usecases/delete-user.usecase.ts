208e0ec457ad4317f105bd10037a48da65503985
João Vitor <joaovitorcfdev@gmail.com>
2023-08-08 11:51:45 -0300

    adding DeleteUserUseCase

--- src/users/application/usecases/delete-user.usecase.ts
+++ src/users/application/usecases/delete-user.usecase.ts
@@ -1,20 +1,18 @@
 import { UserRepository } from '@/users/domain/repositories/user.repository';
-import { UserOutput, UserOutputMapper } from '../dto/user-output';
 import { UseCase as DefaultUseCase } from '@/shared/application/usecases/use-case';
 
-export namespace GetUseCase {
+export namespace DeleteUserUseCase {
   export type Input = {
     id: string;
   };
 
-  export type Output = UserOutput;
+  export type Output = void;
 
   export class UseCase implements DefaultUseCase<Input, Output> {
     constructor(private userRepository: UserRepository.Repository) {}
 
     async execute(input: Input): Promise<Output> {
-      const entity = await this.userRepository.findById(input.id);
-      return UserOutputMapper.toOutput(entity);
+      await this.userRepository.delete(input.id);
     }
   }
 }
